b3c6418c4ae3b84543618b2f4e48d6081db835d0
emabytes <emadavor22@gmail.com>
2020-07-24 11:28:18 +0200

    first

---
+++ main.js
@@ -0,0 +1,30 @@
+const singers = [
+	{ name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" },
+	{ name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
+	{ name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B"},
+	{ name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock"},
+	{ name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica"},
+	{ name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock"},
+	{ name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop"},
+	{ name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock"},
+];
+  
+// Sortiere die Bands alphabetisch nach Namen.
+
+singers.sort(function (a, b) {
+    let nameA = a.name;
+    console.log("nameA -> " + nameA)
+    let nameB = b.name;
+    console.log("nameB -> " + b.name)
+
+    if (nameA < nameB) {
+        return -1;
+    }
+    else if (nameA > nameB) {
+        return 1;
+    } else {
+        return 0; //if equal names
+    }
+});
+
+console.log(singers);
